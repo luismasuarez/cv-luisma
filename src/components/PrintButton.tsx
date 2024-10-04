369ccb5f2ecde1445f16c0df7f95f618e9cf8776
Luisma <luismasuarezzz@gmail.com>
2024-10-04 14:00:32 -0400

    fix pritn button onClick

--- src/components/PrintButton.tsx
+++ src/components/PrintButton.tsx
@@ -7,7 +7,7 @@ type Props = {
 const PrintButton = ({ reactToPrintFn }: Props) => {
   return (
     <button
-      onClick={reactToPrintFn}
+      onClick={() => reactToPrintFn()}
       className="mt-4 py-2 px-6 rounded-md bg-slate-500 text-white font-bold hover:bg-slate-600 transition duration-200"
     >
       Imprimir CV
